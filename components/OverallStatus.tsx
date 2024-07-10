ca345a1ecb67be1ae547278a3381364839433cad
He Hao Yang <hehaoyang699@gmail.com>
2024-07-10 14:15:35 +0800

    Update OverallStatus.tsx

--- components/OverallStatus.tsx
+++ components/OverallStatus.tsx
@@ -1,25 +1,5 @@
 import { Center, Title } from '@mantine/core'
 import { IconCircleCheck, IconAlertCircle } from '@tabler/icons-react'
-import { useEffect, useState } from 'react';
-
-function useWindowVisibility() {
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    const handleVisibilityChange = () => {
-      console.log('visibility change', document.visibilityState);
-      setIsVisible(document.visibilityState === 'visible');
-    };
-
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-
-    return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
-  }, []);
-
-  return isVisible;
-}
 
 export default function OverallStatus({
   state,
@@ -29,34 +9,17 @@ export default function OverallStatus({
   let statusString = ''
   let icon = <IconAlertCircle style={{ width: 64, height: 64, color: '#b91c1c' }} />
   if (state.overallUp === 0 && state.overallDown === 0) {
-    statusString = 'No data yet'
+    statusString = '尚无信息'
   } else if (state.overallUp === 0) {
-    statusString = 'All systems not operational'
+    statusString = '所有系统均检测异常'
   } else if (state.overallDown === 0) {
-    statusString = 'All systems operational'
+    statusString = '所有系统均正常运转'
     icon = <IconCircleCheck style={{ width: 64, height: 64, color: '#059669' }} />
   } else {
-    statusString = `Some systems not operational (${state.overallDown} out of ${state.overallUp + state.overallDown})`
+    statusString = `部分系统异常: (${state.overallDown} 个系统异常，共${
+      state.overallUp + state.overallDown
+    }个系统)`
   }
-
-  const [openTime] = useState(Math.round(Date.now() / 1000))
-  const [currentTime, setCurrentTime] = useState(Math.round(Date.now() / 1000))
-  const isWindowVisible = useWindowVisibility();
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (!isWindowVisible) {
-        return
-      }
-      if (currentTime - state.lastUpdate > 300 && currentTime - openTime > 30) {
-        // trigger a re-fetch
-        window.location.reload()
-      }
-      setCurrentTime(Math.round(Date.now() / 1000))
-    }, 1000)
-
-    return () => clearInterval(interval)
-  })
 
   return (
     <>
@@ -65,8 +28,10 @@ export default function OverallStatus({
         {statusString}
       </Title>
       <Title mt="sm" style={{ textAlign: 'center', color: '#70778c' }} order={5}>
-        Last updated on:{' '}
-        {`${new Date(state.lastUpdate * 1000).toLocaleString()} (${currentTime - state.lastUpdate} sec ago)`}
+        数据更新时间:{' '}
+        {`${new Date(state.lastUpdate * 1000).toLocaleString()} (${
+          Math.round(Date.now() / 1000) - state.lastUpdate
+        } 秒之前)`}
       </Title>
     </>
   )
