5e654f07fe4a0875f74a5a36e77b0dbd35908a1b
hehysh <hehysh@163.com>
2024-07-10 20:00:18 +0800

    :(

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 
-import { Inter } from 'next/font/google'
 import { MonitorState, MonitorTarget } from '@/uptime.types'
 import { KVNamespace } from '@cloudflare/workers-types'
 import { pageConfig, workerConfig } from '@/uptime.config'
